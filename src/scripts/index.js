00d41f1bc32356db683bc709f27fd9144c6ede5e
Eduardo221916 <100217071+Eduardo221916@users.noreply.github.com>
2025-02-25 17:43:15 +0100

    feat:form (#5)

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,5 @@
 import "./src/components/text/text.js";
 import "../components/input/input.js";
 import "../components/textarea/textarea.js";
+import "../components/button-component/button-component.js";
+import "../components/form/form.js";
