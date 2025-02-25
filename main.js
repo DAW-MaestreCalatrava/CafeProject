7266292aa63123ec043be4d5315ccf402399c1e1
Osmagtor <osmagtor01@educastillalamancha.es>
2025-02-25 17:48:48 +0100

    Feature/header (#7)
    * fix: button-component
    
    * feat: header

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ import "./src/components/text/text.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
 
-    // TODO: initialize javascript components.
+    const buttons = [
+        { active: true, text: 'Inicio', link: '/'},
+        { active: false, text: 'Contact con nosotros', link: '/contacta'},
+        { active: false, text: 'Conócenos', link: '/conocer'}
+    ];
 
+    const header = document.querySelector('header-component');
+    header.buttons = buttons;
 });
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,4 +2,5 @@ import "./src/components/text/text.js";
 import "../components/input/input.js";
 import "../components/textarea/textarea.js";
 import "../components/button-component/button-component.js";
+import "../components/header/header.js";
 import "../components/form/form.js";
