76afc0ab2c80ad59f09d454e17f301feb2a909be
osmagtor <osmagtor@gmail.com>
2025-02-25 17:47:18 +0100

    feat: header

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,4 @@
 import "../components/input/input.js";
 import "../components/textarea/textarea.js";
+import "../components/button-component/button-component.js";
+import "../components/header/header.js";
