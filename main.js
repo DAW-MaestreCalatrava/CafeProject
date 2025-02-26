0fc3e030314b9d57e40f899c9f4eacbedd1684a8
13Mihai_Lau <mlaudoiu@gmail.com>
2025-02-26 20:44:48 +0100

    feat: Section2-component

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
-import "./src/components/text/text.js";
+import "./src/scripts/index.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
 
-    const buttons = [
-        { active: true, text: 'Inicio', link: '/'},
-        { active: false, text: 'Contact con nosotros', link: '/contacta'},
-        { active: false, text: 'Conócenos', link: '/conocer'}
-    ];
-
-    const header = document.querySelector('header-component');
-    header.buttons = buttons;
 });
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,9 @@
 import "../components/icons/icons.js";
 import "../components/profile/profile.js";
-import "./src/components/text/text.js";
+import "../components/text/text.js";
 import "../components/input/input.js";
 import "../components/textarea/textarea.js";
 import "../components/button-component/button-component.js";
 import "../components/header/header.js";
 import "../components/form/form.js";
+import "../components/section2-component/section2.js";
