850b38bf92e52e5980e6a65e3218fc6770c7ec93
Osmagtor <osmagtor01@educastillalamancha.es>
2025-02-28 17:03:06 +0100

    fix: header links were missing a dot (#21)

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     const buttons = [
         { text: 'Home', link: '/', active: url.includes('index') || url === '/' },
-        { text: 'About', link: '/about.html', active: url.includes('about') },
-        { text: 'Contact', link: '/contact.html', active: url.includes('contact') }
+        { text: 'About', link: './about.html', active: url.includes('about') },
+        { text: 'Contact', link: './contact.html', active: url.includes('contact') }
     ];
 
     header.buttons = buttons;
