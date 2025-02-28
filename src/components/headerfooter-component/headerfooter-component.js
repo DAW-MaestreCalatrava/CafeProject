e1a3e3ed07926370ebd45ae4669ebb54b3f950ff
13Mihai_Lau <79136198+MihaiGabrielLaudoiu@users.noreply.github.com>
2025-02-28 19:31:47 +0100

    feat: added ht headerfooter component that combines the header and footer components (#29)

--- src/components/headerfooter-component/headerfooter-component.js
+++ src/components/headerfooter-component/headerfooter-component.js
@@ -1,5 +1,6 @@
-import styles from  "./headerfooter.css" with {type:"css"};
-class headerfooter extends HTMLElement {
+import styles from "./headerfooter.css" with { type: "css" };
+
+class HeaderFooter extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
@@ -7,17 +8,32 @@ class headerfooter extends HTMLElement {
   }
 
   render() {
+    // Render the component's HTML structure
     this.shadowRoot.adoptedStyleSheets.push(styles);
     this.shadowRoot.innerHTML = /*html*/ `
       <header-component img="../src/img/logo.jpg"></header-component>
       <slot></slot>
       <my-footer></my-footer>
-        `;
+    `;
+
+    // Execute after DOM is loaded to handle button logic
+    document.addEventListener("DOMContentLoaded", async () => {
+      const header = this.shadowRoot.querySelector('header-component');
+      const url = window.location.pathname.split('/').pop();
+
+      const buttons = [
+        { text: 'Home', link: './', active: !url },
+        { text: 'About', link: './about.html', active: url.includes('about') },
+        { text: 'Contact', link: './contact.html', active: url.includes('contact') }
+      ];
+
+      // Set the buttons array to the header component
+      header.buttons = buttons;
+    });
   }
 
   static get observedAttributes() {
     return [];
-
   }
 
   attributeChangedCallback() {
@@ -25,4 +41,4 @@ class headerfooter extends HTMLElement {
   }
 }
 
-customElements.define("headerfooter-component", headerfooter);
+customElements.define("headerfooter-component", HeaderFooter);
